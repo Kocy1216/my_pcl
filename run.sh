0cda6b80d888e2d454e091bcd887acb17d591b35
TND <taneda-y@roboken.iit.tsukuba.ac.jp>
2022-09-23 09:46:25 +0900

    complete

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-roscore
-rosrun urg_node urg_node 
+cd ~/RTKLIB/app/consapp/str2str/gcc && ./str2str -in ntrip://rtk2go.com:2101/KANAME-ANX-RTCM3 -out serial://ttyACM0:230400
+roslaunch ublox_gps ublox_device.launch node_name:=ublox param_file_name:=zed_f9p_rover
+rosrun satfix_to_pose_py gps_sub.py
+rosrun urg_node urg_node _serial_port:="/dev/ttyACM1"
 rosrun laser_to_points laser_to_points 
-rosrun my_pcl calc_area 
 rosrun limit_pc limit_pc 
+rosrun my_pcl calc_area 
 rviz
--- src/calc_area.cpp
+++ src/calc_area.cpp
@@ -18,7 +18,12 @@
 #include <Eigen/Geometry>
 //#include "handmade_ndt_scan_matcher/ndt_scan_matcher.hpp"
 
-ros::Publisher cloud_pub1,cloud_pub2,pose_pub,pose_pub2,area_pub;
+ros::Publisher cloud_pub1,cloud_pub2,pose_pub,pose_pub2,area_pub,volume_pub;
+
+std::vector<float> area_array;
+geometry_msgs::PoseStamped last_pose;
+bool first_gnss = true;
+float total_volume = 0;
 
 void
 CloudCallback (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
@@ -49,8 +54,16 @@ CloudCallback (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
   outlier_msg.header.frame_id = "laser";
   cloud_pub1.publish (outlier_msg);
 
+  pcl::PointCloud<pcl::PointXYZ>::Ptr limit_outlier_cloud {new pcl::PointCloud<pcl::PointXYZ>};
+  for(int i = 0 ; i < outlier_cloud->points.size() ; i++)
+  {
+    if(abs(outlier_cloud->points[i].y)< 0.25){
+      limit_outlier_cloud->points.push_back(outlier_cloud->points[i]);
+    }
+  }
+
   float area_size = 0.0f;
-  if(outlier_cloud->points.size()>0){
+  if(limit_outlier_cloud->points.size()>0){
     Eigen::Vector3f e,p,r,base_p;
     e = Eigen::Vector3f::Zero();
     e.x() = coefficients->values[4];
@@ -76,31 +89,60 @@ CloudCallback (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 
     p = Eigen::Vector3f::Zero();
     r = Eigen::Vector3f::Zero();
-    std::vector<Eigen::Vector3f> d(outlier_cloud->points.size(), Eigen::Vector3f::Zero());
-    std::vector<Eigen::Vector3f> l(outlier_cloud->points.size(), Eigen::Vector3f::Zero());
+    std::vector<Eigen::Vector3f> d(limit_outlier_cloud->points.size(), Eigen::Vector3f::Zero());
+    std::vector<Eigen::Vector3f> l(limit_outlier_cloud->points.size(), Eigen::Vector3f::Zero());
     float l_all = 0;
-    for(size_t i=0;i<outlier_cloud->points.size();i++){
-      p.x() = outlier_cloud->points[i].x;
-      p.y() = outlier_cloud->points[i].y;
+    for(size_t i=0;i<limit_outlier_cloud->points.size();i++){
+      p.x() = limit_outlier_cloud->points[i].x;
+      p.y() = limit_outlier_cloud->points[i].y;
       r = p - base_p;
       d[i] = r.dot(e)*e;
       l[i] = r - d[i];  
     }
-    std::cout << " d.norm : " << d[0].norm() << std::endl;
-    std::vector<float> s(outlier_cloud->points.size()-1, 0);
+    // std::cout << " d.norm : " << d[0].norm() << std::endl;
+    std::vector<float> s(limit_outlier_cloud->points.size()-1, 0);
     Eigen::Vector3f l_dist;
     l_dist = Eigen::Vector3f::Zero();
-    for(size_t i=0;i<outlier_cloud->points.size()-1;i++){
+    for(size_t i=0;i<limit_outlier_cloud->points.size()-1;i++){
       l_dist = l[i+1] - l[i];
       l_all += l_dist.norm();
       s[i] = ((d[i].norm() + d[i+1].norm()) / 2.0f) * l_dist.norm();
       area_size += s[i];
     }
-    std::cout << " l_all : " << l_all << std::endl;
+    // std::cout << " l_all : " << l_all << std::endl;
   }
   std_msgs::Float32 area_size_msg;
   area_size_msg.data = area_size;
   area_pub.publish(area_size_msg);
+  area_array.push_back(area_size);
+}
+
+void
+PoseCallback (const geometry_msgs::PoseStampedConstPtr& pose_msg)
+{
+  if(!first_gnss && area_array.size()>1){
+    float distance_x,distance_y,gnss_distance,lidar_distance,gnss_volume;
+    gnss_volume = 0;
+    distance_x = pose_msg->pose.position.x - last_pose.pose.position.x;
+    distance_y = pose_msg->pose.position.y - last_pose.pose.position.y;
+    gnss_distance = std::hypot(distance_x,distance_y);
+    lidar_distance = gnss_distance / (area_array.size()-1);
+    std::vector<float> lidar_volume(area_array.size()-1,0);
+    for(size_t i=0;i<area_array.size()-1;i++){
+        lidar_volume[i] = ((area_array[i] + area_array[i+1]) / 2)*lidar_distance;
+        gnss_volume += lidar_volume[i];
+        total_volume += lidar_volume[i];
+    }
+    std_msgs::Float32 total_volume_msg;
+    total_volume_msg.data = total_volume;
+    volume_pub.publish(total_volume_msg);
+    float last_lidar_area;
+    last_lidar_area = area_array[area_array.size()-1];
+    area_array.clear();
+    area_array.push_back(last_lidar_area);
+  }
+  last_pose = *pose_msg;
+  first_gnss = false;
 }
 
 int
@@ -114,7 +156,8 @@ main (int argc, char** argv)
   ros::param::set("down_rate", 0.5);
 
   // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe ("limit_cloud", 1, CloudCallback);
+  ros::Subscriber cloud_sub = nh.subscribe ("limit_cloud", 1, CloudCallback);
+  ros::Subscriber pose_sub = nh.subscribe ("gnss_pose", 1, PoseCallback);
 
   // Create a ROS publisher for the output point cloud
   cloud_pub1 = nh.advertise<sensor_msgs::PointCloud2> ("line_cloud", 1);
@@ -122,6 +165,7 @@ main (int argc, char** argv)
   pose_pub = nh.advertise<geometry_msgs::PoseStamped> ("e", 1);
   pose_pub2 = nh.advertise<geometry_msgs::PoseStamped> ("o", 1);
   area_pub = nh.advertise<std_msgs::Float32>("area_size", 1);
+  volume_pub = nh.advertise<std_msgs::Float32>("total_volume", 1);
   // Spin
   ros::spin ();
 }
